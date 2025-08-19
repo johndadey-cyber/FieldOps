e00a5eb8542972df588b4a973a160493f38062f1
johndadey-cyber <john.dadey@conversamos360.com>
2025-08-19 10:16:01 -0500

    Remove merge conflict markers from local env

--- config/local.env.php
+++ config/local.env.php
@@ -2,9 +2,6 @@
 // Define variables (do NOT return an array)
 $DB_HOST = '127.0.0.1';
 $DB_PORT = '8889';
-<<<<<<< HEAD
 $DB_NAME = 'fieldops_test';
-=======
->>>>>>> d101e92e8c7b7734a2eb996d3725beaddeecd844
 $DB_USER = 'root';
 $DB_PASS = 'root';
