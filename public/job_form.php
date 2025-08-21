a5fd6494b5dde7e7f21382bf3729f7c55423016c
johndadey-cyber <john.dadey@conversamos360.com>
2025-08-21 04:30:54 -0500

    Fetch checklist templates from database

--- public/job_form.php
+++ public/job_form.php
@@ -170,7 +170,7 @@ function stickyArr(string $name, array $default = []): array {
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
   <script>
-    window.jobChecklistTemplates = <?= json_encode(JobChecklistItem::defaultTemplates()); ?>;
+    window.jobChecklistTemplates = <?= json_encode(JobChecklistItem::defaultTemplates($pdo)); ?>;
     window.initialChecklistItems = <?= json_encode($existingChecklist); ?>;
   </script>
   <script src="js/job_form.js"></script>
